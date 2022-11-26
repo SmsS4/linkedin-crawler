22870f490924d668254646f1ff62e23e5f5cb8b7
SmsS4 <smss.lite@gmail.com>
2022-11-26 17:42:40 +0330

    Refactor

--- linkedin/linkedin/main.py
+++ linkedin/linkedin/main.py
@@ -14,13 +14,13 @@ from linkedin import models
 from linkedin import raw_stocks
 
 
-def get_db(user: str, password: str, name: str, host: str, port: int) -> Session:
+def get_db(settings: Dynaconf) -> Session:
     info = {
-        "user": user,
-        "pw": password,
-        "db": name,
-        "host": host,
-        "port": port,
+        "host": settings.db_host,
+        "port": settings.db_port,
+        "db": settings.db_name,
+        "user": settings.db_user,
+        "pw": settings.db_password,
     }
     sqlalchemy_database_url = (
         "postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s" % info
@@ -47,7 +47,7 @@ def clean_name(name: str) -> str:
     return name
 
 
-def main():
+def parse() -> tuple[argparse.Namespace, Dynaconf]:
     parser = argparse.ArgumentParser(description="Run crawler")
     parser.add_argument(
         "--config", "-c", type=str, help="Path to config file", default="settings.toml"
@@ -74,13 +74,10 @@ def main():
         ],
     )
     settings.validators.validate()
-    db = get_db(
-        settings.db_user,
-        settings.db_password,
-        settings.db_name,
-        settings.db_host,
-        settings.db_port,
-    )
+    return args, settings
+
+
+def get_api(settings: Dynaconf) -> Linkedin:
     if settings.get("linkedin_jsessionip", False):
         logger.info("Using cookies")
         cookies = cookiejar_from_dict(
@@ -92,127 +89,161 @@ def main():
         )
         api = Linkedin("", "", cookies=cookies)
     else:
-        logger.warning("Using username and pass")
+        logger.info("Using username and pass")
+        logger.warning("It's better to use cookie to avoid login multiple times")
         api = Linkedin(settings.linkedin_username, settings.linkedin_password)
+    return api
+
+
+def find_company(api: Linkedin, name: str) -> dict | None:
+    name = clean_name(name)
+
+    companies = api.search_companies(name, limit=1)
+    if not len(companies):
+        logger.error("Could not find company %s", name)
+        return None
+    company = companies[0]
+    logger.info(
+        "Found company (for %s) --- name: %s --- headline: %s --- subline: %s",
+        name,
+        company["name"],
+        company["headline"],
+        company["subline"],
+    )
+    return company
+
+
+def find_people(api: Linkedin, urn_id: str, data: dict) -> list:
+    companies_urn = [urn_id]
+    if "affiliatedCompaniesResolutionResults" in data:
+        companies_urn.extend(
+            [
+                x["entityUrn"].split(":")[-1]
+                for x in list(data["affiliatedCompaniesResolutionResults"].values())
+            ]
+        )
+    logger.debug("%d affiliated companies", len(companies_urn))
+    people = api.search_people(
+        current_company=companies_urn, include_private_profiles=False
+    )
+    logger.info("Found %d people", len(people))
+    return people
+
+
+def add_locations(db: Session, urn_id: str, locations: list) -> None:
+    logger.debug("Adding locations")
+    for loc in locations:
+        db.add(
+            models.Locations(
+                company_urn_id=urn_id,
+                country=loc["country"],
+                geographic_area=loc["geographicArea"],
+                city=loc["city"],
+                postal_code=loc["postalCode"],
+                line=loc["line1"],
+                headquarter=loc["headquarter"],
+            )
+        )
+    db.commit()
+
+
+def handle_experience(db: Session, exp: dict):
+    tp = exp.get("timePeriod", None)
+    start = tp.get("startDate", None) if tp else None
+    end = tp.get("endDate", None) if tp else None
+    db.add(
+        models.Experience(
+            location=exp.get("geoLocationName", None),
+            company_name=exp["companyName"],
+            company_urn=exp["companyUrn"].split(":")[-1]
+            if "companyUrn" in exp
+            else None,
+            title=exp["title"],
+            start=start,
+            end=end,
+        )
+    )
+
+
+def handle_education(db: Session, edu: dict):
+    tp = edu.get("timePeriod", None)
+    start = tp.get("startDate", None) if tp else None
+    end = tp.get("endDate", None) if tp else None
+    db.add(
+        models.Education(
+            degree=edu.get("degree", None),
+            activities=edu.get("activities", None),
+            name=edu["schoolName"],
+            field=edu.get("fieldOfStudy", None),
+            start=start,
+            end=end,
+        )
+    )
+
+
+def handle_person(person: dict, api: Linkedin, db: Session) -> None:
+    logger.debug("Getting info of %s", person)
+    profile = api.get_profile(urn_id=person["urn_id"])
+    logger.debug("data: %s", profile)
+    db.add(
+        models.People(
+            industry_name=profile["industryName"],
+            first_name=profile["firstName"],
+            last_name=profile["lastName"],
+            student=profile["student"],
+            country=profile["geoCountryName"],
+            city=profile.get("geoLocationName", None),
+        )
+    )
+    logger.debug("data: %s", profile["education"])
+    for edu in profile["education"]:
+        handle_education(db, edu)
+    logger.debug("data: %s", profile["experience"])
+    for exp in profile["experience"]:
+        handle_experience(db, exp)
+
+
+def handle_stock(db: Session, api: Linkedin, symbol: str, name: str) -> None:
+    logger.info("symbol: %s, name: %s", symbol, name)
+    company = find_company(api, name)
+    if not company:
+        return
+    urn_id = company["urn_id"]
+    data = api.get_company(urn_id)
+    logger.debug("Adding new company")
+    db.add(
+        models.Company(
+            urn_id=urn_id,
+            url=data["url"],
+            staff_count=data["staffCount"],
+            specialities=data["specialities"],
+            name=data["universalName"],
+            symbol=symbol,
+        )
+    )
+    db.commit()
+    add_locations(db, urn_id, data["confirmedLocations"])
+    people = find_people(api, company["urn_id"], data)
+    for person in people:
+        handle_person(person, api, db)
+    db.commit()
+
+
+def main():
+    args, settings = parse()
+    db = get_db(settings)
+    api = get_api(settings)
     if args.cookie:
         print("-----------------------")
         print(
-            f"li_at: {api.client.session.cookies.get('li_at')}\njsessionip: {api.client.session.cookies.get('JSESSIONID')}"
+            f"li_at:      {api.client.session.cookies.get('li_at')}\n"
+            f"jsessionip: {api.client.session.cookies.get('JSESSIONID')}"
         )
         print("-----------------------")
         exit(0)
     stocks = raw_stocks.get_raw_stocks()
     for symbol, name in stocks[3:20]:
-        name = clean_name(name)
-        logger.info("symbol: %s, name: %s", symbol, name)
-        companies = api.search_companies(name, limit=1)
-        if not len(companies):
-            logger.error("Could not find company %s", name)
-            continue
-
-        company = companies[0]
-
-        logger.info(
-            "Found company (for %s) --- name: %s --- headline: %s --- subline: %s",
-            name,
-            company["name"],
-            company["headline"],
-            company["subline"],
-        )
-
-        urn_id = company["urn_id"]
-        data = api.get_company(urn_id)
-
-        logger.debug("Adding new company")
-        db.add(
-            models.Company(
-                urn_id=urn_id,
-                url=data["url"],
-                staff_count=data["staffCount"],
-                specialities=data["specialities"],
-                name=data["universalName"],
-                symbol=symbol,
-            )
-        )
-        db.commit()
-
-        logger.debug("Adding locations")
-        for loc in data["confirmedLocations"]:
-            db.add(
-                models.Locations(
-                    company_urn_id=urn_id,
-                    country=loc["country"],
-                    geographic_area=loc["geographicArea"],
-                    city=loc["city"],
-                    postal_code=loc["postalCode"],
-                    line=loc["line1"],
-                    headquarter=loc["headquarter"],
-                )
-            )
-        db.commit()
-
-        companies_urn = [company["urn_id"]]
-        if "affiliatedCompaniesResolutionResults" in data:
-            companies_urn.extend(
-                [
-                    x["entityUrn"].split(":")[-1]
-                    for x in list(data["affiliatedCompaniesResolutionResults"].values())
-                ]
-            )
-        logger.debug("%d affiliated companies", len(companies_urn))
-
-        people = api.search_people(
-            current_company=companies_urn, include_private_profiles=False
-        )
-        logger.info("Found %d people", len(people))
-
-        for person in people:
-            logger.debug("Getting info of %s", person)
-            profile = api.get_profile(urn_id=person["urn_id"])
-            logger.debug("data: %s", profile)
-            db.add(
-                models.People(
-                    industry_name=profile["industryName"],
-                    first_name=profile["firstName"],
-                    last_name=profile["lastName"],
-                    student=profile["student"],
-                    country=profile["geoCountryName"],
-                    city=profile.get("geoLocationName", None),
-                )
-            )
-            logger.debug("data: %s", profile["education"])
-            for edu in profile["education"]:
-                tp = edu.get("timePeriod", None)
-                start = tp.get("startDate", None) if tp else None
-                end = tp.get("endDate", None) if tp else None
-                db.add(
-                    models.Education(
-                        degree=edu.get("degree", None),
-                        activities=edu.get("activities", None),
-                        name=edu["schoolName"],
-                        field=edu.get("fieldOfStudy", None),
-                        start=start,
-                        end=end,
-                    )
-                )
-            logger.debug("data: %s", profile["experience"])
-            for exp in profile["experience"]:
-                tp = exp.get("timePeriod", None)
-                start = tp.get("startDate", None) if tp else None
-                end = tp.get("endDate", None) if tp else None
-                db.add(
-                    models.Experience(
-                        location=exp.get("geoLocationName", None),
-                        company_name=exp["companyName"],
-                        company_urn=exp["companyUrn"].split(":")[-1]
-                        if "companyUrn" in exp
-                        else None,
-                        title=exp["title"],
-                        start=start,
-                        end=end,
-                    )
-                )
-        db.commit()
+        handle_stock(db, api, symbol, name)
 
 
 # https://github.com/tomquirk/linkedin-api
